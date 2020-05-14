a7f1080cf48be183ae60168524e1f5a1665a547e
Daniyal Guliev <tzota@mail.ru>
2020-05-14 21:06:16 +0300

    update

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail
+#set -euo pipefail
 
 # current dir https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
